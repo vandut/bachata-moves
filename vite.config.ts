e4e340bc66926c82859be4a9783dc686cdebd2fb
vandut <konrad.bielak@gmail.com>
2025-07-20 16:05:22 +0200

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/bachata-moves/',
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
