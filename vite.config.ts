ab49c0b336f1aeb4b0694c2bb76ddb192ea1fca8
vandut <konrad.bielak@gmail.com>
2025-07-30 21:42:58 +0200

    Configure PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,59 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [
+        VitePWA({
+          registerType: 'autoUpdate',
+          devOptions: {
+            enabled: true,
+          },
+          includeAssets: [
+            'icons/favicon.ico',
+            'icons/apple-touch-icon.png',
+            'icons/pwa-192x192.png',
+            'icons/pwa-512x512.png',
+            'icons/pwa-maskable-192x192.png',
+            'icons/pwa-maskable-512x512.png',
+          ],
+          manifest: {
+            name: 'Bachata Moves',
+            short_name: 'Bachata Moves',
+            description: "An application to browse and manage Bachata lessons and figures.",
+            theme_color: '#F9FAFB',
+            background_color: '#f9fafb',
+            icons: [
+              {
+                src: 'icons/pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'any',
+              },
+              {
+                src: 'icons/pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any',
+              },
+              {
+                src: 'icons/pwa-maskable-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'maskable',
+              },
+              {
+                src: 'icons/pwa-maskable-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable',
+              },
+            ],
+          },
+        }),
+      ],
       base: '/bachata-moves/',
       resolve: {
         alias: {
